c8fc135115589b5e3400165f931fc2aff99fd09f
Milee-14 <116631869+Milee-14@users.noreply.github.com>
2022-10-25 15:11:32 +0530

    Update script.js

--- module-4/script.js
+++ module-4/script.js
@@ -30,7 +30,7 @@ WARNING!!! WARNING!!!
 
 (function () {
 
-var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+var names = ["Yash", "Raj", "Atul", "Ankush", "Prankur", "Frank", "Arhat", "Jane", "Laura", "Jim"];
 
 for (var i = 0; i < names.length; i++) {
   var firstLetter = names[i].charAt(0).toLowerCase();
